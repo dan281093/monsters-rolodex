84a52d5c6c2f795dcf34da54c66f8480c17ef6de
dan amouyal <danamouyal@hotmail.fr>
2020-04-23 16:35:25 +0300

    Updates

---
+++ precache-manifest.318df200d4a884e26e23e1f43897c692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ae112e8d99c2a5ceb0ff67c641dc57",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "baf203b193b7e24467f3",
+    "url": "/monsters-rolodex/static/css/main.f9bc9ba4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf203b193b7e24467f3",
+    "url": "/monsters-rolodex/static/js/main.1ebd36e6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
